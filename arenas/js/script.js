a21a0b3a2bbbadc6b8fb563bc95224cd62be3f43
Daniel200x <114607809+Daniel200x@users.noreply.github.com>
2025-08-04 23:33:28 -0300

    n

--- arenas/js/script.js
+++ arenas/js/script.js
@@ -663,3 +663,13 @@ window.addEventListener('resize', function() {
         if (eliminatorias) eliminatorias.classList.add('active');
     }
 });
+
+// Efecto al hacer scroll
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar-fixed');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
